d4e8130f55a3fb18f4f08b1361db9112db780858
John Mettraux <jmettraux@gmail.com>
2026-08-03 10:17:12 +0900

    Scaffold makeElt()

--- src/json_flaneur.js
+++ src/json_flaneur.js
@@ -10,10 +10,59 @@ var JsonFlaneur = (function() {
   //
   // protected functions
 
+  let elementFunctions = {};
+
+  let makeElt = function(tag, atts, text) {
+
+    let m = tag.match(/[#.]?[^#.\s]+/g);
+    let tagname = m.find(e => e.match(/^[^#.]/)) || 'div';
+
+    let e = document.createElement(tagname);
+      //
+    for (let ic of m) {
+      if (ic.match(/^\./)) e.classList.add(ic.slice(1));
+      else if (ic.match(/^#/)) e.id = ic.slice(1);
+    }
+      //
+    for (let k in atts) { e.setAttribute(k, atts[k]); }
+    if (typeof text === 'string') e.innerText = text;
+
+    for (let fname in elementFunctions) {
+      e[fname] = elementFunctions[fname].bind(e);
+    }
+
+    return e;
+  };
+
+  let determineType = function(o) {
+
+    if (o === null) return 'null';
+    if (Array.isArray(o)) return 'array';
+    return (typeof o);
+  };
+
+  //let makeKeyElement = function(t, js) {};
+
+  let makeLeafElement = function(t, js) {
+    return makeElt(
+      `.json_flaneur_leaf.json_flaneur_${t}`, {}, JSON.stringify(js));
+  };
+
+  let makeCollectionElement = function(t, js) {
+    let e = makeElt(`.json_flaneur_collection.json_flaneur_${t}`, {});
+    return e;
+  };
+
   //
   // public functions
 
   this.makeElement = function(js) {
+
+    let t = determineType(js);
+    if (t === 'array' || t === 'object') return makeCollectionElement(t, js)
+    if (t) return makeLeafElement(t, js);
+
+    throw new Error(`JsonFlaneur: cannot make element out of ${typeof js}`);
   };
 
   //
